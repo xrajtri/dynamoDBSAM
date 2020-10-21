4bf2560159e844d0d2f8a2acca78bfac5bd5027f
xrajtri <rajantrivedi1@gmail.com>
2020-10-21 12:42:39 +0530

    Commiting after lambda functions for - delete, update and getAll

--- .aws-sam/build/CreateFunction/index.js
+++ .aws-sam/build/CreateFunction/index.js
@@ -27,7 +27,67 @@ exports.create = async (event) =>{
     }
     catch(err){
         console.log('Error occured'+err);
+        createResponse(200,err);
     }
     console.log("Item created successfully.");
     return createResponse(200,'Item Created Successfully.');
-}
+};
+
+exports.getAll = async (event) =>{
+    let params = {
+        TableName: tableName
+    }
+    let data;
+    try{
+        data = await dynamo.scan(params).promise();
+    }
+    catch(err){
+        console.log("Error occured"+err);
+        createResponse(200,err);
+    }
+    if(!data.Items ){
+        console.log("No items found.");
+        return createResponse(200,'No Items Found');
+    }
+    console.log("Items retrieved successfully.");
+    return createResponse(200,JSON.stringify(data.Items));
+};
+
+exports.update = async (event) =>{
+    let params = {
+        TableName: tableName,
+        Item: JSON.parse(event.body)
+    };
+    let data;
+    try{
+        data = await dynamo.put(params).promise();
+    }
+    catch(err){
+        console.log("Error occured"+err);
+        return createResponse(200,err);
+    }
+    console.log(`Record updated for id = "+${params.Item.todo_id}`);
+    return createResponse(200,`Record updated for todo_id = ${params.Item.todo_id}\n`);
+};
+
+exports.delete = async (event) =>{
+    let eventBody = JSON.parse(event.body);
+    let idToDelete = eventBody.id;
+    console.log("ID to delete = "+idToDelete);
+    let params = {
+        TableName: tableName,
+        Key:{
+            todo_id: idToDelete
+        }
+    };
+    let data;
+    try{
+        data = await dynamo.delete(params).promise();
+    }
+    catch(err){
+        console.log("Error Occured!"+err);
+        return createResponse(200,err);
+    }
+    console.log(`Record deleted for id = ${idToDelete}`);
+    return createResponse(200,`Record deleted for id = ${idToDelete}`);
+};
